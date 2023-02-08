f9d9a5611ebb3bfbf22faad1f01c6ebd2680c7be
goldlilya1612 <goldlilya1612@yandex.com>
2023-02-08 22:10:23 +0300

    refactor: jsx to tsx

---
+++ src/components/ButtonBlock/ButtonBlock.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import "./ButtonBlock.css";
+import {IButtonBlockProps} from "../../interfaces/props/button-block-props.interface";
+
+
+const ButtonBlock: React.FunctionComponent<IButtonBlockProps> = ({
+  content,
+  name,
+  isValid,
+  isDisabled,
+}) => {
+  return (
+    <div
+      className={`section-with-form__wrapper section-with-form__wrapper_${name}`}
+    >
+      {isValid ? (
+        isDisabled ? (
+          <button disabled type="submit" className="section-with-form__button">
+            {content.button}
+          </button>
+        ) : (
+          <button type="submit" className="section-with-form__button">
+            {content.button}
+          </button>
+        )
+      ) : (
+        <button
+          type="submit"
+          className="section-with-form__button section-with-form__button_invalid"
+          disabled
+        >
+          {content.button}
+        </button>
+      )}
+      <p className="section-with-form__text">
+        {content.text}
+        <Link to={content.path} className="section-with-form__text-link">
+          {content.link}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default ButtonBlock;
---
+++ src/interfaces/props/button-block-props.interface.ts
@@ -0,0 +1,8 @@
+export interface IButtonBlockProps {
+  content: {
+    [key: string]: string;
+  };
+  name: string;
+  isValid: boolean;
+  isDisabled: boolean;
+}
