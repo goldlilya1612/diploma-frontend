3df4c66821b0c1c05b9ba0251d43616a4989216d
goldlilya1612 <goldlilya1612@yandex.com>
2023-06-02 15:29:55 +0300

    fix button styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import "./Footer.scss";
 function Footer() {
   return (
     <section className="footer">
-      <p className="footer__text">Диплом Володиной Лилии х Махотиной Ксении.</p>
       <p className="footer__year">© 2023</p>
     </section>
   );
--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -4,9 +4,6 @@ import PromoPicture from "../../images/promo-picture";
 function Promo() {
   return (
     <section className="promo">
-      <h1 className="promo__title">
-        Диплом студентов Володиной Лилии и Махотиной Ксении.
-      </h1>
       <PromoPicture className="promo__picture" />
     </section>
   );
